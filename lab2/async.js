03e67754cb39b553d505be5b01ecab45699c4b92
Saajidh Nawwar <68622804+SaajidNawwar@users.noreply.github.com>
2021-05-16 16:59:02 +0530

    lab 02

---
+++ lab2/async.js
@@ -0,0 +1,12 @@
+//start now, end later
+//will not block the codelines
+
+console.log(1);
+console.log(2);
+
+setTimeout(() =>{
+    console.log('Callback function fired!')
+})
+
+console.log(3);
+console.log(4);
